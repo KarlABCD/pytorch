227952b5e0d83701085dd8a2b43b6aa6a8dd065b
xu.liqing <liqing.xu@enjoymove.com>
2025-02-20 18:23:04 +0800

    更新

--- RL/REINFORCE.py
+++ RL/REINFORCE.py
@@ -40,18 +40,15 @@ class CliffWalkingEnv:
         return self.y * self.ncol + self.x
 
 # 策略网络
-class PolicyNet(nn.Module):
+class PolicyNet(torch.nn.Module):
     def __init__(self, state_dim, hidden_dim, action_dim):
         super(PolicyNet, self).__init__()
-        self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        return F.softmax(x, dim=-1)
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim=1)
 
 # REINFORCE算法实现
 class REINFORCE:
@@ -67,8 +64,8 @@ class REINFORCE:
         
     def state_one_hot(self, state):
         if state.size == 1:
-            state_one_hot = torch.zeros((self.state_dim), dtype = torch.float32)
-            state_one_hot[state] = 1
+            state_one_hot = torch.zeros((state.size, self.state_dim), dtype = torch.float32)
+            state_one_hot[0][state] = 1
         else:
             state_one_hot = torch.zeros((state.size, self.state_dim), dtype = torch.float32)
             for i in range(state.size):
@@ -92,8 +89,8 @@ class REINFORCE:
         for i in reversed(range(len(reward_list))):  # 从最后一步算起
             reward = reward_list[i]
             state = self.state_one_hot(np.array(state_list[i])).to(self.device)
-            action = torch.tensor(action_list[i]).to(self.device)
-            log_prob = torch.log(self.policy_net(state)[action])
+            action = torch.tensor([action_list[i]]).view(-1, 1).to(self.device)
+            log_prob = torch.log(self.policy_net(state).gather(1, action))
             G = self.gamma * G + reward
             loss = -log_prob * G  # 每一步的损失函数
             loss.backward()  # 反向传播计算梯度
@@ -104,7 +101,7 @@ class REINFORCE:
         q_max = q.max()
         a = [0 for _ in range(4)]
         for i in range(4):
-            if(q[i] == q_max):
+            if(q[0][i] == q_max):
                 a[i] = 1
         return a 
 def print_agent(agent, env, action_meaning, disaster=[], end=[]):
@@ -129,8 +126,8 @@ if __name__ == "__main__":
     ncol = 12
     nrow = 4
     env = CliffWalkingEnv(ncol, nrow)
-    gamma = 0.99
-    hidden_dim = 256
+    gamma = 0.98
+    hidden_dim = 128
     state_dim = 12 * 4
     action_dim = 4
     lr = 0.001
